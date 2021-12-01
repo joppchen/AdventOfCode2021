0bac42e0e2c8b29ffa17966dda1a46fb135e0ae3
Jon Petter Grandal <jon.petter.grandal@dnv.com>
2021-12-01 07:31:56 +0100

    Day 1 solved - real ugly.

---
+++ src/AoC2021/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AoC2021
+{
+    class Program
+    {
+        private const int Year = 2021;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello Advent of Code {Year}!");
+
+            //Console.WriteLine("Type in day number to solve: ");
+            //var svada = Console.ReadLine();
+            //var dayNumber = svada;
+
+            Day1.Main.Solve();
+        }
+    }
+}
